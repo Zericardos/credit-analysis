46c03d6740d9cf5e3bbc474cfbbf729cae7d4b4a
José Ricardo Silveira <ricardusilveira@gmail.com>
2023-03-03 00:54:32 -0300

    predição sendo realizada (necessário checar qualidade do resultado

--- modelagem_logistica_simples.R
+++ modelagem_logistica_simples.R
@@ -55,19 +55,25 @@ summary(modelo_atrasos_0_dias)
 summary(modelo_atrasos_90_dias)
 logLik(modelo_atrasos_0_dias)
 logLik(modelo_atrasos_90_dias)
+df_predict <- data.frame(
+  cnae_secao=factor("PJ - Atividades administrativas e serviços complementares", levels=c("-", "PJ - Administração pública, defesa e seguridade social","PJ - Agricultura, pecuária, produção florestal, pesca e aqüicultura","PJ - Água, esgoto, atividades de gestão de resíduos e descontaminação","PJ - Alojamento e alimentação","PJ - Artes, cultura, esporte e recreação","PJ - Atividades administrativas e serviços complementares","PJ - Atividades financeiras, de seguros e serviços relacionados","PJ - Atividades imobiliárias","PJ - Atividades profissionais, científicas e técnicas","PJ - Comércio; reparação de veículos automotores e motocicletas","PJ - Construção","PJ - Educação","PJ - Eletricidade e gás","PJ - Indústrias de transformação","PJ - Indústrias extrativas","PJ - Informação e comunicação","PJ - Outras atividades de serviços","PJ - Saúde humana e serviços sociais","PJ - Transporte, armazenagem e correio")),
+  indexador=factor("Pós-fixado", levels=c("Flutuantes", "Índices de preços", "Outros indexadores", "Pós-fixado", "Prefixado", "TCR/TRFC")),
+  modalidade=factor("PJ - Capital de giro rotativo", levels=c("PF - Cartão de crédito", "PF - Empréstimo com consignação em folha", "PF - Empréstimo sem consignação em folha", "PF - Habitacional", "PF - Outros créditos", "PF - Rural e agroindustrial", "PF - Veículos", "PJ - Capital de giro", "PJ - Capital de giro rotativo", "PJ - Comércio exterior", "PJ - Financiamento de infraestrutura/desenvolvimento/projeto e outros créditos", "PJ - Habitacional", "PJ - Investimento", "PJ - Operações com recebíveis", "PJ - Outros créditos", "PJ - Rural e agroindustrial")),
+  porte=factor("PJ - Médio                                   ", levels=c("PF - Acima de 20 salários mínimos            ", "PF - Até 1 salário mínimo                    ", "PF - Indisponível                            ", "PF - Mais de 1 a 2 salários mínimos          ", "PF - Mais de 10 a 20 salários mínimos        ", "PF - Mais de 2 a 3 salários mínimos          ", "PF - Mais de 3 a 5 salários mínimos          ", "PF - Mais de 5 a 10 salários mínimos         ", "PF - Sem rendimento                          ", "PJ - Grande                                  ", "PJ - Indisponível                            ", "PJ - Médio                                   ", "PJ - Micro                                   ", "PJ - Pequeno                                 ")),
+  ocupacao=factor("PF - Empregado de empresa privada", levels=c("-", "PF - Aposentado/pensionista", "PF - Autônomo", "PF - Empregado de empresa privada", "PF - Empregado de entidades sem fins lucrativos", "PF - Empresário", "PF - MEI", "PF - Outros", "PF - Servidor ou empregado público")),
+  uf=factor("SP", levels=c("AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO", "MA", "MG", "MS", "MT", "PA", "PB", "PE", "PI", "PR", "RJ", "RN", "RO", "RR", "RS", "SC", "SE", "SP", "TO")),
+  TAXAS_DESEMPREGO=4.4,
+  IPCA_TAXA_VARIACAO=0.64,
+  IPCA_INDICE_GERAL=5348.49,
+  INDICE_CONDICOES_ECONOMICAS_ATUAIS=95.71,
+  PIB=625136.3)
 predict(object=modelo_atrasos_0_dias,
-        data.frame(
-          cnae_cecao="PJ - Atividades administrativas e serviços complementares",
-          indexador="Pós-fixado",
-          modalidade="PJ - Capital de giro rotativo",
-          porte="PJ - Médio                                   ",
-          ocupacao="PF - Empregado de empresa privada",
-          uf="SP",
-          TAXAS_DESEMPREGO=4.4,
-          IPCA_TAXA_VARIACAO=0.64,
-          IPCA_INDICE_GERAL=5348.49,
-          INDICE_CONDICOES_ECONOMICAS_ATUAIS=95.71
-          )
+        df_predict,
+        type = "response"
         )
+predict(object=modelo_atrasos_90_dias,
+        df_predict,
+        type = "response"
+)
 
 
